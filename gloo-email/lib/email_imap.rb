41010da4d8dd7b50ebbf2f3d676c3737940ef42a
Eric Crane <encrane@datacor.com>
2026-03-14 16:20:05 -0400

    Update email_imap.rb

--- gloo-email/lib/email_imap.rb
+++ gloo-email/lib/email_imap.rb
@@ -13,6 +13,7 @@ class EmailImap < Gloo::Core::Obj
   PASSWORD = 'password'.freeze
   MAILBOX = 'mailbox'.freeze
   MESSAGES = 'messages'.freeze
+  SEARCH = 'UNSEEN'.freeze
 
   #
   # The name of the object type.
@@ -30,48 +31,38 @@ class EmailImap < Gloo::Core::Obj
   end
 
   # 
-  # Get the email server.
+  # Get the email server host.
   #
   def server
     return find_child_value SERVER
   end
 
-  # Get the email port.
+  #
+  # Get the email server port.
   #
   def port
     return find_child_value PORT
   end
 
+  #
   # Get the email username.
   #
   def username
     return find_child_value USERNAME
   end
 
+  #
   # Get the email password.
   #
   def password
     return find_child_value PASSWORD
   end
 
+  #
   # Get the email mailbox.
   #
   def mailbox
     return find_child_value MAILBOX
-  end
-
-  # Get the email search.
-  #
-  def search
-    return find_child_value SEARCH
-  end
-
-  # Get the email mark as read.
-  #
-  def mark_as_read
-    val = find_child_value MARK_AS_READ
-    puts "Mark as read value: #{val}"
-    return val == 'true'
   end
 
 
@@ -121,31 +112,15 @@ class EmailImap < Gloo::Core::Obj
     imap.select(mailbox)
 
     # Search for messages matching filter
-    ids = imap.search([search])
+    ids = imap.search([SEARCH])
 
     if ids.empty?
       puts "No new messages."
     else
       ids.each do |msg_id|
-        raw_message = imap.fetch(msg_id, "RFC822")[0].attr["RFC822"]
-        mail = Mail.read_from_string(raw_message)
-
-        puts "------------------------"
-        puts "From: #{mail.from.join(', ')}"
-        puts "Subject: #{mail.subject}"
-        puts "Date: #{mail.date}"
-        puts "Body:"
-        puts mail.body.decoded
-        puts "------------------------"
-
-        # # Mark as seen (optional)
-        # if mark_as_read
-        #   puts "Marking message as read"
-        #   imap.store(msg_id, "+FLAGS", [:Seen])
-        # else
-        #   puts "Marking message as unread"
-        #   imap.store(msg_id, "-FLAGS", [:Seen])
-        # end
+        raw_message = imap.fetch( msg_id, "RFC822" )[0].attr["RFC822"]
+        mail = Mail.read_from_string( raw_message )
+        process_message( mail )
       end
     end
 
@@ -154,6 +129,31 @@ class EmailImap < Gloo::Core::Obj
     # -----------------------------
     imap.logout
     imap.disconnect  
+  end
+
+  #
+  # Process a single email message
+  #
+  def process_message( mail )
+    from = mail.from.join(', ')
+    to = mail.to.join(', ')
+    subject = mail.subject
+    dt = mail.date
+    body = mail.body.decoded
+
+    msg_can = find_child MESSAGES
+    
+    msg = msg_can.find_add_child( msg_can.children.length.to_s, 'email' )
+    o = msg.find_add_child( 'from', 'string' )
+    o.set_value from
+    o = msg.find_add_child( 'to', 'string' )
+    o.set_value to
+    o = msg.find_add_child( 'subject', 'string' )
+    o.set_value subject
+    o = msg.find_add_child( 'date', 'string' )
+    o.set_value dt
+    o = msg.find_add_child( 'body', 'text' )
+    o.set_value body
   end
 
 end
